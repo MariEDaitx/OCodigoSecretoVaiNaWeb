1572b0f0ece63175b37ea9e13aa37bf54fafc4bb
MariEDaitx <mariaeduardasantosdaitx@gmail.com>
2025-02-14 15:01:49 -0300

    primeiro commit

---
+++ missão1.py
@@ -0,0 +1,8 @@
+# Missão 1: RESTAURANDO AS REGRAS ESCOLARES
+# O vírus apagou os critérios de aprovação dos alunos! Para ajudar o Professor Byte a organizar o sistema, sua tarefa é criar um programa que verifique se um aluno foi aprovado (nota maior ou igual à 6) ou reprovado (nota menor ou igual à 5)
+aluno = input("Digite sua nota: ")
+
+if aluno >= 6:
+    print("Meus parabéns voce foi aprovado!! :-)")
+else:
+    print("Poxa sinto muito você foi reprovado :-(")
