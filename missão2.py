4199fe1a86724163e0217adc502718ccb281edca
MariEDaitx <mariaeduardasantosdaitx@gmail.com>
2025-02-14 20:01:20 -0300

    segundo commit

--- missão2.py
+++ missão2.py
@@ -1,8 +1,8 @@
 # O Sistema Eleitoral Secreto 
 # O grêmio estudantil da escola realiza votações para decidir melhorias e inovações, mas o vírus desativou a verificação de elegibilidade para votar! Sua tarefa é criar um programa que pergunte a idade do usuário e informe se ele pode votar (mínimo: 16 anos).
-votar = input("Qual é a sua Idade ? ")
+votar = int(input("Qual é a sua Idade ? "))
 
-if votar > 18:
+if votar >= 16:
     print("Você está apto a votar!")
 else: 
     print("Você não está apto a votar!")
---
+++ missão4.py
@@ -0,0 +1,14 @@
+# Restaurando a identificação de Nùmeros
+# Os robôs da escola precisam identificar padrões numéricos para resolver cálculos e otimizar os sistemas. No entanto, o vírus bagunçou os algoritmos e agora eles não conseguem mais somar corretamente!
+# Crie um programa que peça dois números ao usuário e exiba a soma deles.
+
+def calculo_soma(num1, num2):
+    
+    num1 = int(print("Digite o primeiro numero: "))
+    num2 = int(print("Digite o segundo numero: "))
+
+    return num1 + num2
+
+    resultadoSoma = calculo_soma(num1,num2)
+    print("A soma é: ", resultadoSoma)
+
